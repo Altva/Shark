2b572485c2d5b8b3dca885134160dceb7f3bd17f
iMaximuz@gmail.com <imaximuz@gmail.com>
2016-09-17 02:45:24 -0500

    Shark to stronk

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,12 @@
 function openNav(){
     document.getElementById("sideBar").style.width = "100px";
-    document.getElementById("main").style.marginLeft = "100px";
+    //document.getElementById("main").style.marginLeft = "100px";
+    document.getElementById("btnOpen").style.visibility = "hidden";
+
 }
 
 function closeNav(){
     document.getElementById("sideBar").style.width = "0px";
-    document.getElementById("main").style.marginLeft = "0px";
+    //document.getElementById("main").style.marginLeft = "0px";
+    document.getElementById("btnOpen").style.visibility = "visible";
 }
