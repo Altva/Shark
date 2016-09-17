11c822932d9aa55c357b89fa2eadbc8c7626a224
iMaximuz@gmail.com <imaximuz@gmail.com>
2016-09-17 01:03:14 -0500

    Shark web

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("sideBar").style.width = "100px";
+    document.getElementById("main").style.marginLeft = "100px";
+}
+
+function closeNav(){
+    document.getElementById("sideBar").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+}
