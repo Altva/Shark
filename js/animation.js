44fa75eae8e438b53efa194d0960ccbb48235529
iMaximuz@gmail.com <imaximuz@gmail.com>
2016-09-17 05:37:58 -0500

    Icono de menu interactivo

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,26 @@
+var isMenuOpen = true;
+
 function openNav(){
+    isMenuOpen = true;
     document.getElementById("sideBar").style.width = "100px";
     //document.getElementById("main").style.marginLeft = "100px";
-    document.getElementById("btnOpen").style.visibility = "hidden";
+    //document.getElementById("btnOpen").style.visibility = "hidden";
 
 }
 
 function closeNav(){
+    isMenuOpen = false;
     document.getElementById("sideBar").style.width = "0px";
     //document.getElementById("main").style.marginLeft = "0px";
-    document.getElementById("btnOpen").style.visibility = "visible";
+    //document.getElementById("btnOpen").style.visibility = "visible";
+}
+
+function changeMenu(x){
+    x.classList.toggle("change");
+    if(isMenuOpen){
+        closeNav();
+    }
+    else{
+        openNav();
+    }
 }
