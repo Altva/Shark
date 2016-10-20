ecb791a7ef1aeced7775aa8e65201316b8cb1c03
iMaximuz@gmail.com <imaximuz@gmail.com>
2016-10-20 05:59:44 -0500

    Termine el cuadro de notificaciones

--- js/side-bar.js
+++ js/side-bar.js
@@ -1,7 +1,7 @@
 
 $(document).ready( function(){
     var isMenuOpen = false;
-    
+
     function openNav(){
         isMenuOpen = true;
         $("#sideBar").css("width", "100px");
@@ -22,8 +22,9 @@ $(document).ready( function(){
         }
     });
 
-    $('#search-side-button').click(function(){
-        
+
+    $('#search-side-button').click(function(e){
+        e.stopPropagation();
         var $this = $(this);
         var top = $this.offset().top;
         var buttonWidth = $(this).parent().width();
@@ -32,8 +33,39 @@ $(document).ready( function(){
 
         $('#search-bar-dialog').toggleClass("visible");
         $('#search-bar-dialog').css({"top": top, "left": buttonWidth});
+
+        if($('#notification-dialog').hasClass('visible')){
+            $('#notification-dialog').removeClass('visible');
+        }
     });
 
-    $('#notification-dialog');
+    $('#notification-side-button').click(function(e){
+        e.stopPropagation();
+        var $this = $(this);
+        var top = $this.offset().top;
+        var buttonWidth = $(this).parent().width();
 
+        top = top - $(document).scrollTop() - 
+            ($('#notification-dialog').height() * 0.5) + $this.parent().height() * 0.5;
+
+        $('#notification-dialog').toggleClass("visible");
+        $('#notification-dialog').css({"top": top, "left": buttonWidth});
+
+        if($('#search-bar-dialog').hasClass('visible')){
+            $('#search-bar-dialog').removeClass('visible');
+        }
+
+    });
+
+    $(document).click(function (e)
+    {
+        $(".dialog").each(function(){
+            var $this = $(this);
+            if(!$this.is(e.target) && $this.has(e.target).length === 0){
+                if($this.hasClass("visible")){
+                    $this.toggleClass("visible");
+                }
+            }
+        });
+    });
 });
